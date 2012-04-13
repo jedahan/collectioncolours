89e3ff33112c1a057b68b73c6545985e7901456d
Jonathan Dahan <jonathan@jedahan.com>
2012-04-13 11:10:28 -0700

    print an individual images info

--- colours.js
+++ colours.js
@@ -7,14 +7,15 @@ var palette = require('palette')
   , canvas = new Canvas
   , ctx = canvas.getContext('2d')
   , path = process.argv[2]
-  , out = '/tmp/out.png';
+  , xml2js = require('xml2js');
 
 if (!path) {
-  console.error('Usage: test <image>');
+  console.error('Usage: '+process.argv[1]+' <image>');
   process.exit(1);
 }
 
 var img = new Image;
+var imageInfo = {colours: [], url: path, year: 0};
 
 img.onload = function(){
   canvas.width = img.width;
@@ -22,33 +23,31 @@ img.onload = function(){
   ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(img, 0, 0);
-  paintPalette();
-  save();
+  getColours();
 };
 
 img.src = path;
 
-function paintPalette() {
-  var x = 0;
+function getColours() {
   var colors = palette(canvas);
-  var hexcolors = [];
   colors.forEach(function(color){
     var r = color[0]
       , g = color[1]
       , b = color[2]
       , val = r << 16 | g << 8 | b
       , str = '#' + val.toString(16);
-
-    ctx.fillStyle = str;
-    ctx.fillRect(x += 31, canvas.height - 40, 30, 30);
-    hexcolors.push(str);
+    imageInfo.colours.push(str);
   });
-  console.log(hexcolors);
+  parseDate();
 }
 
-function save() {
-  fs.writeFile(out, canvas.toBuffer(), function(err){
-    if (err) throw err;
-    console.log('saved %s', out);
+function parseDate() {
+  var parser = new xml2js.Parser();
+  fs.readFile(path + '.txt', function(err, data) {
+      parser.parseString(data, function (err, result) {
+          imageInfo.year = result['@'].object_date_begin;
+          console.log(imageInfo);
+      });
   });
 }
+
