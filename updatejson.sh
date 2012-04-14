fd890aca458df56ffa47e4464004a9fb2c7291db
Jonathan Dahan <jonathan@jedahan.com>
2012-04-14 14:08:59 -0700

    when processing colours, grab ALL the data

--- updatejson.sh
+++ updatejson.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 echo '[' > imageInfo.json
-for image in img/000/*jpg; do ./colours.js $image >> imageInfo.json; done
+for image in img/*/*jpg; do ./colours.js $image >> imageInfo.json; done
 echo ']' >> imageInfo.json
